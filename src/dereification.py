c68ff72349a90b630297e55c8270adcba0868510
hsolbrig <solbrig@jhu.edu>
2020-08-14 13:42:52 -0500

    Checkpoint - at this point we know that rdflib-jsonld 1.1 does not recognize "@indes"

---
+++ src/dereification.py
@@ -0,0 +1,27 @@
+import os
+from rdflib import Graph
+
+input_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'input'))
+output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'output'))
+
+# Step one -- convert the tags to indices
+g = Graph()
+g.parse(os.path.join(input_dir, 'dereification.json'), format="json-ld",
+       context=os.path.join(input_dir, 'dereification_p1.context.jsonld'))
+output = g.serialize(format='json-ld', auto_compact=True).decode()
+
+# Bug in json-ld processor -- "@index" gets combined with the default namespace...
+output = output.replace('http://example.org/@index', '@index')
+p1_json = os.path.join(output_dir, 'dereification_p1x.jsonld')
+with open(p1_json, 'w') as f:
+       f.write(output)
+
+# Step two -- convert the indices to predicates
+g = Graph()
+g.parse(p1_json, format="json-ld", context=os.path.join(input_dir, 'dereification_p2.context.jsonld'))
+output2 = g.serialize(format='json-ld').decode()
+output2 = output2.replace('http://example.org/@index', '@index')
+
+g = Graph()
+g.parse(data=output2, format='json-ld')
+g.serialize(os.path.join(output_dir, 'dereification.ttl'), format='ttl')
